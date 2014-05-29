657a649dd8480a653062b49cc3cd52621dbc042c
michellepoulin <mmichpp@gmail.com>
2014-05-29 09:36:06 -0600

    another

--- js/main.js
+++ js/main.js
@@ -16,24 +16,24 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function()
-            {
-                self.showAlert('Store initialized', 'Info');
-            });
+        {
+            self.showAlert('Store initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
 
     },
 
     showAlert: function(message, title)
     {
-        if (navigator.notification) 
-        {
-            alert('test');
+        // if (navigator.notification) 
+        // {
+            
             navigator.notification.alert(message, null, title, 'OK');
-        }
-        else 
-        {
-            alert(title ? (title + ": " + message) : message);
-        }
+        // }
+        // else 
+        // {
+        //     alert(title ? (title + ": " + message) : message);
+        // }
     }
 
 };
