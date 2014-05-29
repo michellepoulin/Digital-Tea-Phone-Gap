d1928a13a461db66357a126da1e61b6d41836d52
michellepoulin <mmichpp@gmail.com>
2014-05-29 10:04:50 -0600

    more

--- js/main.js
+++ js/main.js
@@ -12,30 +12,27 @@ var app = {
             }
         });
     },
-
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function()
-        {
-            self.showAlert('Store initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-
-    },
-
-    showAlert: function(message, title)
+    showAlert: function (message, title) 
     {
         if (navigator.notification) 
         {
-
-            navigator.notification.alert(message, NULL, title, 'OK');
+            navigator.notification.alert(message, null, title, 'OK');
         }
         else 
         {
             alert(title ? (title + ": " + message) : message);
         }
-    }
+    },
 
+    initialize: function() 
+    {
+        var self = this;
+        this.store = new MemoryStore(function() 
+        {
+            self.showAlert('Store Initialized', 'Info');
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
 };
 
 app.initialize();
