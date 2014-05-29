b544be0e630f386cfa81180fd2ee9cbc127aafe7
michellepoulin <mmichpp@gmail.com>
2014-05-29 15:15:15 -0600

    saw the alert
    yeah

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ var app = {
         var self = this;
         this.store = new MemoryStore(function()
         {
-            self.showAlert('Store initialized', 'Info');
-            // self.renderHomeView();
+            // self.showAlert('Store initialized', 'Info');
+            self.renderHomeView();
         });
         
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        // $('.search-key').on('keyup', $.proxy(this.findByName, this));
 
     },
 
@@ -36,6 +36,17 @@ var app = {
         {
             alert(title ? (title + ": " + message) : message);
         }
+    },
+    renderHomeView: function()
+    {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 };
 
